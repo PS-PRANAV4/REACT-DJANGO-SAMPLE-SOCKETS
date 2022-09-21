2dcb51d24371728c2afb3ca6235673495b0a1e1d
PS-PRANAV4 <pranavpranab@gmail.com>
2022-09-21 16:52:30 +0530

    first commit

--- core/settings/base.py
+++ core/settings/base.py
@@ -12,9 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
@@ -24,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'django-insecure-l49l+fewn!d0+(z&_@l$&#l112$gi=7sp_)!b4r++k@o^h^y03'
 
 # Application definition
 
@@ -99,7 +97,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'australia/perth'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
